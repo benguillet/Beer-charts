bea818c91a5f59cb077c56f42c6a842b7e207411
Quentin Cuvillier <quentin.cuvillier@utbm.fr>
2013-05-03 18:31:57 +0200

    Moved app.rb up in folder

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require 'haml'
 
-set :views, File.dirname(__FILE__) + "/../views"
-set :public_folder, File.dirname(__FILE__) + '/../resources'
+set :views, File.dirname(__FILE__) + "/views"
+set :public_folder, File.dirname(__FILE__) + '/resources'
 
 post '/' do
   @login = params[:login]
