8f9b2295d7095ddccd5d0b436247b87decbf5ead
Quentin Cuvillier <quentin.cuvillier@utbm.fr>
2013-05-03 18:26:09 +0200

    Add post requet

--- src/app.rb
+++ src/app.rb
@@ -1,8 +1,18 @@
 require 'sinatra'
+require 'haml'
 
 set :views, File.dirname(__FILE__) + "/../views"
 set :public_folder, File.dirname(__FILE__) + '/../resources'
 
+post '/' do
+  @login = params[:login]
+  @pass = params[:mdp]
+end
+
 get '/' do
   haml :index
 end
+
+not_found do  
+  halt 404, 'page not found'  
+end  
