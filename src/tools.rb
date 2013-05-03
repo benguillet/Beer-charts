6584d0586b07becc3950d33fbb291eb7e3bde707
Quentin Cuvillier <quentin.cuvillier@utbm.fr>
2013-05-03 21:06:09 +0200

    Config file is now read only once

--- src/tools.rb
+++ src/tools.rb
@@ -1,10 +1,20 @@
 class Tools
 
+  @config = nil
+
   # Load config from config/app.yml
   def self.load_config config_name
+    
+    # If already loaded then return it
+    if !@config.nil? && !@config[config_name].nil?
+      return @config[config_name]
+    end
+
+    # If not load then read the yaml file 
     config = YAML.load_file("config/app.yml")
-    if config[config_name]
-      return config[config_name]
+    if !config[config_name].nil?
+      @config = config
+      return @config[config_name]
     else
       raise "Error config '#{config_name}' not found in app.yml"
     end
