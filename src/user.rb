f61e56022c4e0250f8d0209f719ec108171b469b
Quentin Cuvillier <quentin.cuvillier@utbm.fr>
2013-05-03 21:00:16 +0200

    Implemented connection method

--- src/user.rb
+++ src/user.rb
@@ -1,6 +1,8 @@
+require 'net/http'
+
 class User
 
-  @connected = false
+  @cookies = nil
 
   def initialize(login, password)
     @login = login
@@ -9,11 +11,23 @@ class User
   end
 
   def connection!
-    #TODO Connect to the website
+    urls = Tools::load_config('url')
+    form = Tools::load_config('form')
+    # Request
+    res = Net::HTTP.post_form(URI(urls['connect']), 'domain' => form['domain'], 
+                                                    'username' => @login,
+                                                    'password' => @password,
+                                                    'connectbtn' => form['btn'])
+    # Save the cookies
+    cookies_array = Array.new
+    all_cookies = res.get_fields('set-cookie')
+    all_cookies.each { | cookie | cookies_array.push(cookie.split('; ')[0]) }
+    @cookies = cookies_array.join('; ') 
   end
 
   def connected?
-    return @connected  
+    config_cookie = Tools::load_config('cookies');
+    @cookies.include? config_cookie['auth_cookie']
   end
 
   def getData criteria
