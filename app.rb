80b833232ce63aa5ba3cef2c5a2cb43e246f06be
Quentin Cuvillier <quentin.cuvillier@utbm.fr>
2013-05-04 21:23:33 +0200

    Ajax

--- app.rb
+++ app.rb
@@ -8,14 +8,12 @@ require './src/user.rb'
 set :views, File.dirname(__FILE__) + "/views"
 set :public_folder, File.dirname(__FILE__) + '/resources'
 
-post '/' do
+post '/data' do
   user = User.new(params[:login], params[:mdp])
   if user.connected?
     data = user.getData(:all).to_json
-    haml :index, :locals => { :logged => true, :data => data }
   else
-    haml :index, :locals => { :logged => false, 
-                              :error  => 'Connexion impossible' }
+    {'error' => 'Une erreur est survenue !'}.to_json
   end
 end
 
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,40 @@
+
+/**
+ * Horrible Javascript, you have been warn !
+ */
+
 $(window).ready(function () {
-  if (data != undefined) {
-    APP.init(); 
-  }
+  google.load('visualization', '1.0', {'packages':['corechart'], callback:APP.init});
 });
 
 var APP = {
 
   init : function() {
-    google.load('visualization', '1.0', {'packages':['corechart'], callback:function() {
-      APP.drawMonthlyBalance();
-    }});
+    $('#submit_form').click(function(){
+      VIEW.save();
+      APP.loadMonthlyBalance();
+      return false;
+    });
   },
 
-  drawMonthlyBalance: function() {
+  loadMonthlyBalance: function() {
+    var login = $('#login').val();
+    var pass  = $('#password').val()
+    VIEW.showLoader();
+    $.post("/data", { login: login, mdp: pass })
+      .done(function(data) {
+        data = JSON.parse(data);
+        if (data.error != undefined) {
+          VIEW.restore(data.error);
+        } else {
+          APP.drawMonthlyBalance(data);
+        }
+      }
+    );
+  },
+
+  drawMonthlyBalance: function(data) {
+
     var gdata = new google.visualization.DataTable();   
     gdata.addColumn('string', 'Date');
     gdata.addColumn('number', 'Euro');
@@ -21,11 +43,39 @@ var APP = {
     $.each(data, function(index, value) {
       gdata.addRow([value.date, parseInt(value.out)])
     });
-      
+    
     var options = { 'width':1000, 'height':400 };
+    VIEW.clear();
+    
     newdiv = document.createElement('div');
     $('body').append(newdiv); 
     var chart = new google.visualization.AreaChart(newdiv); 
     chart.draw(gdata, options);
+
   }
+}
+
+var VIEW = {
+
+    clear: function() {
+      document.body.innerHTML = '';
+    },
+
+    save : function() {
+      VIEW.body = document.body.innerHTML;
+    },
+
+    restore : function() {
+      document.body.innerHTML = VIEW.body;
+      APP.init();
+    },
+
+    showLoader : function() {
+      VIEW.clear();
+      var img = new Image();
+      img.src = '/images/loader.gif';
+      img.style.marginLeft = '30%';
+      document.body.appendChild(img);
+    },
+
 }
